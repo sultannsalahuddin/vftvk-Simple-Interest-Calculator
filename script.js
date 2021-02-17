c4c6121f2d443ccb98d08ea57d639d5627ae27f8
sultan <sultanstj@gmail.com>
2021-02-17 14:36:57 +0000

    modified files

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal=document.getElementById("principal").value;
+    var rate=document.getElementById("rateVal").value;
+    var years=document.getElementById("years").value;
+    var interest=(principal*rate*years)/100;
+    document.getElementById("result").innerHTML= interest;
+    var output="";
+    output+="<p>If you deposit <mark>"+principal+"</mark><br>";
+    output+="at an interest rate of <mark>"+rate+"%."+"</mark><br>";
+    output+="You will recieve an amount of <mark>"+interest+"</mark><br>";
+    output+="in the year <mark>"+(parseInt(years)+2021)+"</mark>.";
+    document.getElementById("results").innerHTML=output;
+}    
+function rateChange(val){
+    document.getElementById("rateVal").innerHTML=val;
+}       
+function warn(){
+    var x =  parseInt(document.getElementById("principal").value);
+    if (x <= 1)
+    {
+    alert("Enter a positive number!");
+    return false;
+    }
 }
-        
